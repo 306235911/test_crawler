cb13a65a80117500ea0d6a0da11af286dff00cbb
weixiong <weixiong@dataw.com.cn>
2018-09-10 16:25:36 +0800

    init

---
+++ src/crawler/tutorial/tutorial/spiders/tutorial_spider.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Created by weixiong
+
+import scrapy
+
+
+class TutorialSpider(scrapy.Spider):
+    name = "tutorial"
+
+    def start_requests(self):
+        urls = [
+            'http://quotes.toscrape.com/page/1/',
+            'http://quotes.toscrape.com/page/2/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
