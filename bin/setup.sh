18948ae7d80e42f064db3f35afb6c66c9a3d33d6
weixiong <weixiong@dataw.com.cn>
2018-09-11 10:30:40 +0800

    test

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,7 +3,7 @@ if [[ `pwd` == *'test_crawler/bin' ]]; then
 fi
 
 git pull
-rm -rf .venv && virtualenv --no-site-packages --python=python3.7 .venv
-source .venv/bin/activate && pip install -r requirements.txt
+#rm -rf .venv && virtualenv --no-site-packages --python=python3.7 .venv
+#source .venv/bin/activate && pip install -r requirements.txt
 #cd src && python setup.py install
 #cd ../bin && sh stop.sh && sh start.sh
