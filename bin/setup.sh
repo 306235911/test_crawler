905286cdb610d5a18e5de08aabbd13d386fdc06d
weixiong <weixiong@dataw.com.cn>
2018-09-11 11:13:46 +0800

    test

--- bin/setup.sh
+++ bin/setup.sh
@@ -4,6 +4,7 @@ fi
 
 git pull
 cd src/crawler/tutorial
+scrapy crawl tutorial
 #rm -rf .venv && virtualenv --no-site-packages --python=python3.7 .venv
 #source .venv/bin/activate && pip install -r requirements.txt
 #cd src && python setup.py install
