6e6d56c401da6a55f35a72ea0939d86178ab7b08
weixiong <weixiong@dataw.com.cn>
2018-09-12 15:53:10 +0800

    kafka

--- src/dealer/kafka/kafka2hbase.py
+++ src/dealer/kafka/kafka2hbase.py
@@ -13,11 +13,11 @@ def consumer():
     # 为kafka动态分区所用到的group name
     group_id = "test_group"
     # 用于反序列化数据的方法
-    value_deserializer = lambda v: json.dumps(v)
+    # value_deserializer = lambda v: json.dumps(v)
     # kafka读取数据时最小的返回数据量
     fetch_min_bytes = 1
     consumer = KafkaConsumer(kafka_topic, group_id=group_id, bootstrap_servers=bootstrap_servers,
-                             value_deserializer=value_deserializer, fetch_min_bytes=fetch_min_bytes)
+                             fetch_min_bytes=fetch_min_bytes)
     for msg in consumer:
         print(msg)
 
