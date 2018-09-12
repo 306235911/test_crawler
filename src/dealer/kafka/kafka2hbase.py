a704b1929a12474fa8c10b617f510543e51f8b51
weixiong <weixiong@dataw.com.cn>
2018-09-12 16:00:41 +0800

    kafka

--- src/dealer/kafka/kafka2hbase.py
+++ src/dealer/kafka/kafka2hbase.py
@@ -9,16 +9,19 @@ def consumer():
     # 获取数据的kafka topic
     kafka_topic = "test"
     # kafka 的节点
-    bootstrap_servers = "localhost:9092"
+    bootstrap_servers = ["localhost:9092"]
     # 为kafka动态分区所用到的group name
     group_id = "test_group"
     # 用于反序列化数据的方法
     # value_deserializer = lambda v: json.dumps(v)
     # kafka读取数据时最小的返回数据量
-    fetch_min_bytes = 1
-    consumer = KafkaConsumer(kafka_topic, group_id=group_id, bootstrap_servers=bootstrap_servers,
-                             fetch_min_bytes=fetch_min_bytes)
+    # fetch_min_bytes = 1
+    consumer = KafkaConsumer(kafka_topic, group_id=group_id, bootstrap_servers=bootstrap_servers)
     for msg in consumer:
-        print(msg)
+        print(msg.topic)
+        print(msg.partition)
+        print(msg.offset)
+        print(msg.key)
+        print(msg.value)
 
 consumer()
