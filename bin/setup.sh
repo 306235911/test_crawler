4dc9a0ee7eec05f21eddac01a76c46447bc77c89
weixiong <weixiong@dataw.com.cn>
2018-09-14 16:44:59 +0800

    bug

--- bin/setup.sh
+++ bin/setup.sh
@@ -5,7 +5,7 @@ fi
 git pull
 source .venv/bin/activate
 cd src && python setup.py install
-cd common_crawler/instance && python test_schduler.py
+cd scheduler && python test_schduler.py
 #scrapy crawl tutorial
 #rm -rf .venv && virtualenv --no-site-packages --python=python3.7 .venv
 #source .venv/bin/activate && pip install -r requirements.txt
--- src/scheduler/test_schduler.py
+++ src/scheduler/test_schduler.py
@@ -4,8 +4,10 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
+from common_crawler.instance.tutorial.spiders.tutorial_spider import TutorialSpider
+
 process = CrawlerProcess(get_project_settings())
 
 # 'followall' is the name of one of the spiders of the project.
-process.crawl('tutorial')
+process.crawl(TutorialSpider)
 process.start() # the script will block here until the crawling is finished
