f35dc6eb9f74e3483e959b3a5b64f7f62418d4e3
weixiong <weixiong@dataw.com.cn>
2018-09-14 13:56:43 +0800

    setup

--- src/dealer/log/logger.py
+++ src/dealer/log/logger.py
@@ -4,7 +4,7 @@
 import logging
 from logging.handlers import TimedRotatingFileHandler, WatchedFileHandler
 
-LOGGER_FILE_PATH = "/home/pwx/log/"
+LOGGER_FILE_PATH = "/home/pwx/log/log.log"
 LOGGER_FORMAT = '[%(asctime)s] [%(name)s:%(lineno)d] [%(levelname)s] %(message)s'
 
 
