8e08ba56aa65e293ab40ff45cbecc0c6b2f11506
weixiong <weixiong@dataw.com.cn>
2018-09-17 14:41:53 +0800

    log

--- src/scheduler/task_producer.py
+++ src/scheduler/task_producer.py
@@ -16,24 +16,24 @@ redis = Redis(host="localhost", port=6379, db=1)
 
 def worker(spider):
     crawler_prosecc = CrawlerProcess(get_project_settings())
-
     crawler_prosecc.crawl(spider)
+    logger.info("start spider %s" % spider.__class__.__name__)
     crawler_prosecc.start()  # the script will block here until the crawling is finished
 
 
 def up_worker():
     redis_task_queue = "crawler:task:queue"
-    # todo:log
     while (True):
         interval = 10
         task = redis.lpop(redis_task_queue)
         if task:
+            logger.info("worker get task")
             spider_class = importlib.import_module(task.rsplit(".", 1)[0])
             spider = getattr(spider_class, task.rsplit(".", 1)[1])()
             worker(spider)
             interval = 1
+        logger.info("sleep %d s" % interval)
         time.sleep(interval)
-
 
 
 if __name__ == '__main__':
