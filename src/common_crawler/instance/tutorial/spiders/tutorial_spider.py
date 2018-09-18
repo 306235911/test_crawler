c5000d90639f5cb518fbaf2b9ad8cb5ed1ceed83
weixiong <weixiong@dataw.com.cn>
2018-09-18 16:33:26 +0800

    start

--- src/common_crawler/instance/tutorial/spiders/tutorial_spider.py
+++ src/common_crawler/instance/tutorial/spiders/tutorial_spider.py
@@ -46,7 +46,7 @@ class TutorialSpider(scrapy.Spider):
             return loader.load_item()
 
     def start_hook(self):
-        process = CrawlerProcess(get_project_settings())
+        process = CrawlerProcess()
         # 'followall' is the name of one of the spiders of the project.
         process.crawl(self.__class__)
         logger.info("now to start")
