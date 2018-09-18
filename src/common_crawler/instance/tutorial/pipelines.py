219f679dddd603522db1238a2b7e0244a456b20b
weixiong <weixiong@dataw.com.cn>
2018-09-18 17:09:35 +0800

    start

--- src/common_crawler/instance/tutorial/pipelines.py
+++ src/common_crawler/instance/tutorial/pipelines.py
@@ -41,6 +41,7 @@ class TutorialPipeline(object):
         try:
             topic = "test"
             item = dict(item)
+            logger.info(item)
             # 指定了用来序列化消息记录的类，如果有key的话还可以指定一个用来序列化key的类(方法?)key_serializer
             self.producer = KafkaProducer(value_serializer=lambda v: json.dumps(v).encode('utf-8'))
             self.producer.send(topic, item)
