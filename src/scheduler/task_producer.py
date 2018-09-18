2147f1190184576aceaacd8c5cc8bdd2e2e5ebee
weixiong <weixiong@dataw.com.cn>
2018-09-18 18:08:33 +0800

    exception

--- src/scheduler/task_producer.py
+++ src/scheduler/task_producer.py
@@ -6,6 +6,7 @@ import time
 
 from redis import Redis
 
+from common_crawler.instance.tutorial.spiders.tutorial_spider import TutorialSpider
 from dealer.log.logger import get_logger
 
 logger = get_logger("worker")
@@ -13,23 +14,27 @@ redis = Redis(host="localhost", port=6379, db=1)
 
 
 def worker(spider):
+
     pass
 
 
 def up_worker():
-    redis_task_queue = "crawler:task:queue"
-    while (True):
-        interval = 10
-        task = redis.lpop(redis_task_queue)
-        if task:
-            logger.info("worker get task")
-            spider_class = importlib.import_module(task.decode("utf-8").rsplit(".", 1)[0])
-            spider = getattr(spider_class, task.decode("utf-8").rsplit(".", 1)[1])()
-            spider.start_hook()
-            # worker(spider)
-            interval = 1
-        logger.info("sleep %d s" % interval)
-        time.sleep(interval)
+    spider_class = importlib.import_module("common_crawler.instance.tutorial.spiders.tutorial_spider")
+    spider = getattr(spider_class, "TutorialSpider")()
+    spider.start_hook()
+
+    # redis_task_queue = "crawler:task:queue"
+    # while (True):
+    #     interval = 10
+    #     task = redis.lpop(redis_task_queue)
+    #     if task:
+    #         logger.info("worker get task")
+    #         spider_class = importlib.import_module(task.decode("utf-8").rsplit(".", 1)[0])
+    #         spider = getattr(spider_class, task.decode("utf-8").rsplit(".", 1)[1])()
+    #         # worker(spider)
+    #         interval = 1
+    #     logger.info("sleep %d s" % interval)
+    #     time.sleep(interval)
 
 
 if __name__ == '__main__':
