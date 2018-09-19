258be1c1ff5a2fc868e8c9c3a0dcb7f7fc3eced5
weixiong <weixiong@dataw.com.cn>
2018-09-19 11:45:02 +0800

    task

--- src/scheduler/task_producer.py
+++ src/scheduler/task_producer.py
@@ -4,12 +4,12 @@
 import importlib
 import time
 import traceback
+from multiprocessing import Process
 
 from redis import Redis
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
-from common_crawler.instance.tutorial.spiders.tutorial_spider import TutorialSpider
 from dealer.log.logger import get_logger
 
 logger = get_logger("worker")
@@ -17,33 +17,37 @@ redis = Redis(host="localhost", port=6379, db=1)
 
 
 def worker(spider):
-
     pass
 
 
 def up_worker():
+    def run_task(task_class):
+        try:
+            process = CrawlerProcess(get_project_settings())
+            process.crawl(task_class)
+            process.start()
+        except Exception as e:
+            logger.error(traceback.format_exc())
+
     redis_task_queue = "crawler:task:queue"
-    process = CrawlerProcess(get_project_settings())
-    try:
-        while (True):
-            interval = 10
-            task = redis.lpop(redis_task_queue)
-            if task:
-                logger.info("worker get task")
-                spider_class = importlib.import_module(task.decode("utf-8").rsplit(".", 1)[0])
-                spider = getattr(spider_class, task.decode("utf-8").rsplit(".", 1)[1])()
-                process.crawl(spider)
-                process.start(stop_after_crawl=False)
-                # worker(spider)
-                interval = 1
-            logger.info("sleep %d s" % interval)
-            time.sleep(interval)
-    except Exception:
-        process.stop()
-        logger.error(traceback.format_exc())
-    finally:
-        if process:
-            process.stop()
+    while (True):
+        interval = 10
+        task = redis.lpop(redis_task_queue)
+        if task:
+            logger.info("worker get task")
+            spider_class = importlib.import_module(task.decode("utf-8").rsplit(".", 1)[0])
+            spider = getattr(spider_class, task.decode("utf-8").rsplit(".", 1)[1])()
+            # process = CrawlerProcess(get_project_settings())
+            # process.crawl(spider)
+            # process.start()
+            p = Process(target=run_task(), args=(spider,))
+            p.start()
+            p.join()
+            logger.info("task finished")
+            # worker(spider)
+            interval = 1
+        logger.info("sleep %d s" % interval)
+        time.sleep(interval)
 
 
 if __name__ == '__main__':
