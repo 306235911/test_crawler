b69e1b35f8e6d0fb981e97efa1dc081caf0aee71
weixiong <weixiong@dataw.com.cn>
2018-09-20 10:05:22 +0800

    task

--- src/scheduler/task_producer.py
+++ src/scheduler/task_producer.py
@@ -4,7 +4,7 @@
 import importlib
 import time
 import traceback
-from multiprocessing import Pool
+from multiprocessing import Process
 
 from redis import Redis
 from scrapy.crawler import CrawlerProcess
@@ -15,28 +15,32 @@ from dealer.log.logger import get_logger
 logger = get_logger("worker")
 redis = Redis(host="localhost", port=6379, db=1)
 
+"""
+    twisted 本身是多线程的，所以在外层使用多线程会 raise Exception，所以这里不用线程池
+"""
+
 
 def worker(spider):
     pass
 
 
-def run_task(task_class):
-    try:
-        process = CrawlerProcess(get_project_settings())
-        process.crawl(task_class)
-        process.start()
-    except Exception as e:
-        logger.error(traceback.format_exc())
+# def run_task(task_class):
+#     try:
+#         process = CrawlerProcess(get_project_settings())
+#         process.crawl(task_class)
+#         process.start()
+#     except Exception as e:
+#         logger.error(traceback.format_exc())
 
 
 def up_worker():
-    # def run_task(task_class):
-    #     try:
-    #         process = CrawlerProcess(get_project_settings())
-    #         process.crawl(task_class)
-    #         process.start()
-    #     except Exception as e:
-    #         logger.error(traceback.format_exc())
+    def run_task(task_class):
+        try:
+            process = CrawlerProcess(get_project_settings())
+            process.crawl(task_class)
+            process.start()
+        except Exception as e:
+            logger.error(traceback.format_exc())
 
     redis_task_queue = "crawler:task:queue"
     spider_list = []
@@ -49,18 +53,20 @@ def up_worker():
             spider = getattr(spider_class, task.decode("utf-8").rsplit(".", 1)[1])()
             spider_list.append(spider)
             interval = 1
-        # reactor不允许 restart，所以通过子进程跑
-        # 判断若没有任务或pool已满就开跑一波
-        if (interval == 10 and len(spider_list) > 0) or len(spider_list) == 2:
-            pool = Pool(2)
-            # 只能 pickle top-level 的方法
-            pool.map(run_task, spider_list)
-            pool.close()
-            pool.join()
+            # reactor不允许 restart，所以通过子进程跑
+            # 判断若没有任务或pool已满就开跑一波
+
+            # twisted 本身是多线程的，所以在外层使用多线程会 raise Exception，所以这里
+            # if (interval == 10 and len(spider_list) > 0) or len(spider_list) == 2:
+            #     pool = Pool(2)
+            #     # 只能 pickle top-level 的方法
+            #     pool.map(run_task, spider_list)
+            #     pool.close()
+            #     pool.join()
             # 可调用本地方法
-            # p = Process(target=run_task, args=(spider,))
-            # p.start()
-            # p.join()
+            p = Process(target=run_task, args=(spider,))
+            p.start()
+            p.join()
             logger.info("task finished")
             # worker(spider)
             spider_list = []
