0fa36ddb0321a0a5b09135f65d78ec271dae10d1
weixiong <weixiong@dataw.com.cn>
2018-09-24 17:26:09 +0800

    hbase

---
+++ src/dealer/hbase/hbase_to_txt.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Created by weixiong
+import happybase
+
+
+def to_hbase():
+    connection = happybase.Connection('localhost', autoconnect=False)
+    connection.open()
+    table = connection.table('testtable')
+    for key, data in table.scan():
+        print(str(key), str(data))
+        break
